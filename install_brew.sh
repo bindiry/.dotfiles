200a25252f6c15c0b4af776508f99d66807c6013
bindiry <bindiry@gmail.com>
2016-06-04 17:40:39 +0800

    update

--- install_brew.sh
+++ install_brew.sh
@@ -15,7 +15,19 @@ vim +PlugInstall +qall
 brew instal dnsmasq
 #cp /usr/local/opt/dnsmasq/dnsmasq.conf.example /usr/local/etc/dnsmasq.conf
 #sudo brew services start dnsmasq
+# install mpv
+brew install mpv --with-bundle
+brew linkapps mpv
+EXTS=( 3GP ASF AVI FLV M4V MKV MOV MP4 MPEG MPG MPG2 MPG4 RMVB WMV MTS )
+brew install duti
+for ext in ${EXTS[@]}
+do
+	lower=$(echo $ext | awk '{print tolower($0)}')
+	duti -s io.mpv $ext all
+	duti -s io.mpv $lower all
+done
 
+# install apps
 brew tap caskroom/cask
 brew cask install google-chrome
 brew cask install the-unarchiver
